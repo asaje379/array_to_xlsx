96948f14ee46269431d101117b3f15999a666457
asaje379 <salemaffa@gmail.com>
2023-09-12 10:48:19 +0100

    chore(): update version

--- dist/lib.d.ts
+++ dist/lib.d.ts
@@ -1,3 +1,3 @@
-import { ConvertOptions, DataSheets, DefaultDataType } from "./interfaces";
+import { ConvertOptions, DataSheets, DefaultDataType } from './interfaces';
 export declare function convertToSheet($data: Record<string, DefaultDataType>[], options?: ConvertOptions): Promise<void | import("exceljs").Buffer>;
-export declare function convertToSheets($data: DataSheets[], options?: Omit<ConvertOptions, "headers">): Promise<void | import("exceljs").Buffer>;
+export declare function convertToSheets($data: DataSheets[], options?: Omit<ConvertOptions, 'headers'>): Promise<void | import("exceljs").Buffer>;
